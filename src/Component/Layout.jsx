a86a37b922a07598099bd4d8581460537d58a6ab
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-10-26 15:01:12 +0530

    add route

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Footer from "./footer";
+import Footer from './Footer'
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
+     <Footer/>
     </>
   );
 };
