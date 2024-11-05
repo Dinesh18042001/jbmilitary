8365babd23516390215674242e53bbbb0d23f3e4
Webpristine <120642477+Webpristine@users.noreply.github.com>
2024-11-05 13:01:35 +0530

    Create bottom menu in mobile view

--- src/Component/BottomHeader.jsx
+++ src/Component/BottomHeader.jsx
@@ -12,16 +12,17 @@ export default function BottomHeader() {
                         <Link to="/"><span><img src="./assets/icon/homeb.png" alt="" /></span> Home </Link>
                     </div>
                     <div className="link">
-                        <a href="#"><span><img src="./assets/icon/shopping-bag.png" alt="" /></span> Shop </a>
+                        <Link to="productdetails"><span><img src="./assets/icon/shopping-bag.png" alt="" /></span> Shop </Link>
                     </div>
+                   
                     <div className="link">
-                        <a href="#"><span><img src="./assets/icon/auction.png" alt="" /></span> Auction </a>
+                        <a href="#"><span><img src="./assets/icon/loupe.png" alt="" /></span> Search </a>
                     </div>
                     <div className="link">
                         <a href="#"><span><img src="./assets/icon/shopping-cart.png" alt="" /></span> Cart </a>
                     </div>
                     <div className="link">
-                        <a href="#"><span><img src="./assets/icon/loupe.png" alt="" /></span> Search </a>
+                        <a href="#"><span><img src="./assets/icon/auction.png" alt="" /></span> Auction </a>
                     </div>
                 </div>
             </div>
--- src/Component/MobileMenu.jsx
+++ src/Component/MobileMenu.jsx
@@ -1,101 +1,101 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
+import { Offcanvas } from 'bootstrap';
 
 export default function MobileMenu() {
+
+  const hideOffcanvas = () => {
+    const offcanvasElement = document.getElementById('offcanvasExample');
+    const offcanvasInstance = Offcanvas.getInstance(offcanvasElement);
+    if (offcanvasInstance) {
+      offcanvasInstance.hide();
+    }
+
+    // Remove the 'show' class and set display to 'none' for the backdrop
+    const backdrop = document.querySelector('.offcanvas-backdrop');
+    if (backdrop) {
+      backdrop.classList.remove('show');
+      backdrop.style.display = 'none';
+    }
+  };
+
   return (
-   <>
-    <div className="offcanvas offcanvas-end mobil_menu_wrapper d-lg-none d-block" tabindex="-1" id="offcanvasExample"
-    aria-labelledby="offcanvasExampleLabel">
-    <div className="offcanvas-header">
-      <h5 className="offcanvas-title" id="offcanvasExampleLabel">
-        <img src="./assets/logo.png" alt="logo" className="img-fluid" />
-      </h5>
-      <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
+    <>
+      <div className="offcanvas offcanvas-end mobil_menu_wrapper d-lg-none d-block" tabIndex="-1" id="offcanvasExample"
+           aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+            <img src="./assets/logo.png" alt="logo" className="img-fluid" />
+          </h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
 
-    <div className="accordion accordion-flush" id="accordionFlushExample">
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-            <Link to ="/">Home</Link>
-          </button>
-        </h2>
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-            <Link to ="#">Shop</Link>
-          </button>
-        </h2>
-      </div>
+        <div className="accordion accordion-flush" id="accordionFlushExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="/" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>Home</Link>
+            </h2>
+          </div>
 
-      
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="productdetails" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>Shop</Link>
+            </h2>
+          </div>
 
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="flush-headingfour">
-          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-            data-bs-target="#flush-collapsefour" aria-expanded="false" aria-controls="flush-collapsefour">
-              Medals
-          </button>
-        </h2>
-        <div id="flush-collapsefour" className="accordion-collapse collapse" aria-labelledby="flush-headingfour"
-          data-bs-parent="#accordionFlushExample">
-          <div className="accordion-body">
-            <div className="col">
-              <Link to ="customizemedal" className="drop-link d-flex allign-items-center text-decoration-none text-dark">
-                
-                <div className="drop_content">
-                Customize Medal
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="flush-headingfive">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapsefive" aria-expanded="false" aria-controls="flush-collapsefive">
+                Medals
+              </button>
+            </h2>
+            <div id="flush-collapsefive" className="accordion-collapse collapse" aria-labelledby="flush-headingfive"
+                 data-bs-parent="#accordionFlushExample">
+              <div className="accordion-body">
+                <div className="col">
+                  <Link to="customizemedal" className="drop-link d-flex align-items-center text-decoration-none text-dark"
+                        onClick={hideOffcanvas}>
+                    <div className="drop_content">Customize Medal</div>
+                  </Link>
                 </div>
-              </Link>
+              </div>
             </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="#" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>Upcoming Auctions</Link>
+            </h2>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="#" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>Selling?</Link>
+            </h2>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="#" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>Sold Items</Link>
+            </h2>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="#" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>About</Link>
+            </h2>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <Link to="contact" className="accordion-button accordion-button-2" onClick={hideOffcanvas}>
+                Contact <i className="fa-solid fa-arrow-right"></i>
+              </Link>
+            </h2>
           </div>
         </div>
       </div>
-
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-          <Link to ="#"> Upcoming Auctions</Link>
-          </button>
-        </h2>
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-          <Link to ="#">Selling?</Link>
-          </button>
-        </h2>
-      </div>
-
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-            <Link to ="#">Sold Items</Link>
-          </button>
-        </h2>
-      </div>
-
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-            <Link to ="#">About</Link>
-          </button>
-        </h2>
-      </div>
-
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button className="accordion-button accordion-button-2" type="button">
-          <Link to="contact">
-                  Contact <i className="fa-solid fa-arrow-right"></i>
-                </Link>
-          </button>
-        </h2>
-      </div>
-    </div>
-  </div>
-
-   </>
-  )
+    </>
+  );
 }
